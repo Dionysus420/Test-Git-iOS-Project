1f2a70e370e0c4af5fe148eb721f4292231ca36c
Richard Holder <Richard@BlackRepublic.local>
2014-09-30 13:17:06 -0400

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Richard Holder on 9/30/14.
+//  Copyright (c) 2014 Richard Holder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Richard Holder on 9/30/14.
+//  Copyright (c) 2014 Richard Holder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
