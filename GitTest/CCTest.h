66313fc3062af2ed8b1179a7ef1bfad1c18711aa
Dionysus420 <Richard@BlackRepublic.local>
2014-11-07 11:40:30 -0500

    added a test class

---
+++ GitTest/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by Richard Holder on 11/7/14.
+//  Copyright (c) 2014 Richard Holder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ GitTest/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by Richard Holder on 11/7/14.
+//  Copyright (c) 2014 Richard Holder. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
